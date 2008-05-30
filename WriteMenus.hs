ccdbe1ed236c61e12a7c3ee205eeb149ce402b3c
Chris Done <chrisdone@gmail.com>
2008-05-30 17:59:44 +0100

    Removed testing code.

--- WriteMenus.hs
+++ WriteMenus.hs
@@ -10,23 +10,23 @@ writeMenus :: IO ()
 writeMenus = grabChapters >>= format
 
 format :: Contents -> IO ()
-format = foldM_ write Nothing . tails . drop 200 . contFlatten where
-    write p []             = return $ Nothing
+format = foldM_ write Nothing . tails . contFlatten where
     write p (c@(ch,_,_):n) = do 
       putStrLn $ prettyHtmlFragment $ 
                showPrevious p +++ showCurChap ch +++ showNext n
       return $ Just c
+    write p _ = return $ Nothing
 
 showCurChap :: String -> Html
 showCurChap = (h2 <<)
 
 showPrevious :: Maybe (String,String,FilePath) -> Html
-showPrevious Nothing = noHtml
 showPrevious (Just (_,title,url)) = p << (ahref url "Previous" +++ title)
+showPrevious _ = noHtml
 
 showNext :: [(String,String,FilePath)] -> Html
 showNext ((_,title,url):_) = p << (ahref url "Next" +++ title)
-showNext _                 = noHtml
+showNext _ = noHtml
 
 contFlatten :: Contents -> [(String,String,FilePath)]
 contFlatten = concatMap flatten where
