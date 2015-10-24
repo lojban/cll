dbb04fcbb0d919cb7fcdaa75347e647c62ca0b36
Gleki Arxokuna <gleki.is.my.name@gmail.com>
2015-10-24 09:49:46 +0300

    sync from docbook-prince

---
+++ scripts/xhtml_postprocess.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd $1
+
+# Conveniently, all the HTML we produce is in a flat directory
+# structure, so making HTML5 app cache manifest files is actually
+# pretty trivial.
+
+echo 'CACHE MANIFEST' >cll.appcache
+find . -type f | sed 's;^\./;;' | grep -v '\.appcache' >>cll.appcache
+
+find . -type f | xargs grep -l '<html xmlns=' | while read file
+do
+  sed -i 's;<html xmlns=;<html xmlns:mml="http://www.w3.org/1998/Math/MathML" manifest="cll.appcache" xmlns=;' "$file"
+done
