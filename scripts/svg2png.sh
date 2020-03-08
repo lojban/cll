56219766498f68dc15222a8b95c8c34718eac80a
lagleki <gleki.is.my.name@gmail.com>
2020-03-08 00:23:08 -0800

    use SVG pictures from la ninpre

---
+++ scripts/svg2png.sh
@@ -0,0 +1,7 @@
+# npm install svgexport -g
+
+for file in orig/chapter-abstractions.svg orig/chapter-connectives.svg orig/chapter-relative-clauses.svg orig/chapter-2-diagram.svg orig/chapter-anaphoric-cmavo.svg orig/chapter-grammars.svg orig/chapter-selbri.svg orig/chapter-about.svg orig/chapter-catalogue.svg orig/chapter-lujvo.svg orig/chapter-tour.svg
+do
+echo "converting "$PWD/$file
+svgexport $PWD/$file $PWD/$file.png 400:
+done
